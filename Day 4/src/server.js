ca3b0a319d8f0fe69e6755e602f602fd9a13b2a5
OSoufian <74008427+OSoufian@users.noreply.github.com>
2025-10-07 16:56:46 +0200

    Système d'authentification

--- Day 4/src/server.js
+++ Day 4/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import usersRouter from "./routers/users-router.js";
+import authenticationRouter from "./routers/authentification-router.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRouter);
+app.use("/authentification", authenticationRouter);
 
 export default app;
